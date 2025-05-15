415b88687b6c8efc53da38f6d8ea8135966c711f
Daniel Kolocka <kolockadaniel@hotmail.com>
2025-05-14 19:34:20 -0700

    update game logic to end when score of 5 reached, add play again functionality, and add css for improved ui

--- javascript.js
+++ javascript.js
@@ -34,10 +34,17 @@ function computerWin() {
     computerScore++;
 }
 
+function tieRound() {
+    results.textContent = "Tie round!"
+}
+
 const scoreBoard = document.querySelector(".score");
 
 function updateScore() {
     scoreBoard.textContent = `Human: ${humanScore} | Computer: ${computerScore}`;
+    if (humanScore >= 5 || computerScore >= 5) {
+        gameOver();
+    }
 }
 
 function playRound(humanChoice) {
@@ -72,20 +79,44 @@ function playRound(humanChoice) {
 
     updateScore();
   }
+
+  const buttons = document.querySelector(".buttons");
+
+  const playAgainBtn = document.createElement("button");
+  playAgainBtn.setAttribute("id", "playAgain");
+  playAgainBtn.textContent = "Play Again";
+  playAgainBtn.setAttribute("style", "font-weight: 500; border: 1px solid transparent; border-radius: 2px;  height: 50px;  line-height: 34px;  font-size: 18px;  color: #ffffff; background-color: #007c89; padding: 1px 18px;");
+
+
+  function gameOver() {
+    buttons.removeChild(rockBtn);
+    buttons.removeChild(paperBtn);
+    buttons.removeChild(scissorsBtn);
+
+    if (humanScore > computerScore) {
+        results.textContent = "Congratulations! You have beat the computer.";
+    }
+    else {
+        results.textContent = "Better luck next time! The computer has beat you.";
+    }
+    buttons.appendChild(playAgainBtn);
+
+  }
   
 
-//   function playGame() {
-//     for (let i=0; i<5; i++) {
-//         const humanSelection = getHumanChoice();
-//         const computerSelection = getComputerChoice();
-  
-//         playRound(humanSelection, computerSelection);
-//     }
-//     console.log("Human Score: " + humanScore);
-//     console.log("Computer Score: " + computerScore);
-//   }
+  function playAgain() {
+    buttons.removeChild(playAgainBtn);
 
-//   playGame();
+    buttons.appendChild(rockBtn);
+    buttons.appendChild(paperBtn);
+    buttons.appendChild(scissorsBtn);
+
+   humanScore = 0;
+   computerScore = 0;
+
+   scoreBoard.textContent = "Human: 0 | Computer: 0";
+   results.textContent = "Begin the game!"
+  }
 
 const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
@@ -94,18 +125,22 @@ const results = document.querySelector(".results");
 
 rockBtn.addEventListener("click", (e) => {
     // console.log(e.target.innerText);
-    let choice = e.target.innerText;
-    playRound(choice);
+    // let choice = e.target.innerText;
+    playRound("rock");
 });
 
 paperBtn.addEventListener("click", (e) => {
-    // console.log(e.target.innerText);
-    let choice = e.target.innerText;
-    playRound(choice);
+    // console.log(e);
+    // let choice = e.target.innerText;
+    playRound("paper");
 });
 
 scissorsBtn.addEventListener("click", (e) => {
     // console.log(e.target.innerText);
-    let choice = e.target.innerText;
-    playRound(choice);
+    // let choice = e.target.innerText;
+    playRound("scissors");
+});
+
+playAgainBtn.addEventListener("click", () => {
+    playAgain();
 });
